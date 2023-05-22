7ddd3f7c446e0f31600e14be519b6a228ffc4681
sushil37 <sushil.shrestha3000@gmail.com>
2023-05-22 11:51:04 +0545

    setup project on react typescript with vite

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      // Add custom rules or override existing rules as needed
+      // Example:
+      // 'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test']],
+    },
+  };
+  
